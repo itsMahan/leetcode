611ddc6492363c00097333375b8e0c5b8a557a3d
mahan <ataeifarmahan@gmail.com>
2025-12-16 16:05:51 +0330

    ❌

---
+++ easy/121.Best_Time_to_Buy_and_Sell_Stock/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
+
+        return max_profit
+
+
+s = Solution()
+print(s.maxProfit([2,1,2,0,1]))
